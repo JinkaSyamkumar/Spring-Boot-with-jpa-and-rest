765061807a8e0394d32aa8ac4cc5e1df9c204c90
syamkumarj <syamkumarj@172.20.10.9>
2017-09-26 00:40:00 +0530

    java revisit

---
+++ JavaRevise/src/com/learning/A_SimpleJavaClass.java
@@ -0,0 +1,20 @@
+
+package com.learning; //package declaration
+
+
+public class A_SimpleJavaClass { //class declaration
+
+	
+	public static void main(String[] args) { //main method
+		//public - means METHOD is accessible to entire application
+		//static - means this METHOD can be called directly from class definition rather than by instance of the class
+		//void - mean this method wont written anything
+		//main name must
+		//String[] - expects array of strings
+		//args - user defined, not necessarily "args", can be anything
+		
+		System.out.println();//executable body
+		System.out.println(Runtime.getRuntime().maxMemory());
+	}
+
+}
